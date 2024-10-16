ffe6019c6ec01cc2568806e81d2cb334f6135a58
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-10-16 19:51:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,6 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7289136166:AAGPpHK4yz99XD5x6JJixPyTlHZvjlFC5aI")
     API_ID = int(os.environ["API_ID", 20346550]
     API_HASH = os.environ["API_HASH", "bc79c3bea7a626887bdc0871eecf0327"]
-    AUTH_USERS = "7081036509"""
+    AUTH_USERS = int(os.environ["AUTH_USERS", 1280494242]
+    """
 
